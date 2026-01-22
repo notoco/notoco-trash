2ae679b93dab9fb0417d2e2242d1cfb4c12829eb
notoco <36659110+notoco@users.noreply.github.com>
2026-01-22 16:31:04 +0100

    up

--- plugin.video.spryciarze/addon.py
+++ plugin.video.spryciarze/addon.py
@@ -12,6 +12,7 @@ import xbmc
 import re
 import ssl
 from urllib.parse import urlencode, parse_qsl, urljoin
+import urllib.parse
 try:
     from urllib.request import urlopen, Request
 except ImportError:
@@ -22,6 +23,7 @@ _addon = xbmcaddon.Addon()
 BASE_URL = 'https://www.spryciarze.pl'
 
 MAIN_CATEGORIES = [
+    {'name': 'Szukaj', 'url': '', 'type': 'search'},
     {'name': 'Komputery', 'url': 'https://komputery.spryciarze.pl/kategorie', 'type': 'category'},
     {'name': 'Kulinaria', 'url': 'https://kulinaria.spryciarze.pl/kategorie', 'type': 'category'},
     {'name': 'Kobieta', 'url': 'https://kobieta.spryciarze.pl/kategorie', 'type': 'category'},
@@ -190,6 +192,9 @@ def list_categories():
         if category['type'] == 'category':
             url = get_url(action='subcategories', url=category['url'])
             is_folder = True
+        elif category['type'] == 'search':
+            url = get_url(action='search')
+            is_folder = False # Search itself is an action, not a folder to list
         else:
             url = get_url(action='videos', url=category['url'])
             is_folder = True
@@ -225,20 +230,35 @@ def list_subcategories(url):
     xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     xbmcplugin.endOfDirectory(_handle)
 
-def list_videos(url, page=1):
-    if page > 1:
-        url = url.rstrip('/') + '/page:' + str(page)
+import urllib.parse
 
-    html = fetch_page(url)
+# ... (rest of the file)
+
+def list_videos(url, page=1, is_search=False):
+    fetch_url = url
+    if page > 1:
+        if is_search:
+            # For search results, append ?page=X
+            # Ensure we don't duplicate query parameters if 'url' already has them
+            url_parts = list(urllib.parse.urlparse(url))
+            query = dict(urllib.parse.parse_qsl(url_parts[4]))
+            query['page'] = str(page)
+            url_parts[4] = urllib.parse.urlencode(query)
+            fetch_url = urllib.parse.urlunparse(url_parts)
+        else:
+            # For categories, append /page:X
+            fetch_url = url.rstrip('/') + '/page:' + str(page)
+
+    html = fetch_page(fetch_url)
     if not html:
         xbmcgui.Dialog().notification('Spryciarze.pl', 'Blad pobierania filmow', xbmcgui.NOTIFICATION_ERROR)
         xbmcplugin.endOfDirectory(_handle, succeeded=False)
         return
 
-    xbmcplugin.setPluginCategory(_handle, 'Filmy')
+    xbmcplugin.setPluginCategory(_handle, 'Wyniki wyszukiwania' if is_search else 'Filmy')
     xbmcplugin.setContent(_handle, 'videos')
 
-    videos = parse_videos(html, url)
+    videos = parse_videos(html, fetch_url)
 
     for video in videos:
         list_item = xbmcgui.ListItem(label=video['title'])
@@ -250,14 +270,31 @@ def list_videos(url, page=1):
 
         list_item.setProperty('IsPlayable', 'true')
 
-        url = get_url(action='play', url=video['url'])
-        xbmcplugin.addDirectoryItem(_handle, url, list_item, False)
-
-    if len(videos) >= 12:
+        play_url = get_url(action='play', url=video['url'])
+        xbmcplugin.addDirectoryItem(_handle, play_url, list_item, False)
+    
+    # Check if there's a next page link
+    match_next_page = re.search(r'data-page-next="(\d+)"', html)
+    if match_next_page:
+        next_page = int(match_next_page.group(1))
         list_item = xbmcgui.ListItem(label='[Nastepna strona]')
-        url_base = url.split('/page:')[0] if '/page:' in url else url
-        url = get_url(action='videos', url=url_base, page=str(page + 1))
-        xbmcplugin.addDirectoryItem(_handle, url, list_item, True)
+        
+        # Determine the base URL for pagination
+        if is_search:
+            # For search results, if 'page' is already in the URL, remove it for the base, then re-add
+            url_obj = list(urllib.parse.urlparse(url))
+            query_params = dict(urllib.parse.parse_qsl(url_obj[4]))
+            query_params.pop('page', None) # Remove existing page param
+            url_obj[4] = urllib.parse.urlencode(query_params)
+            url_base_for_next_page = urllib.parse.urlunparse(url_obj)
+            
+            # Now add the page parameter back for the next page
+            next_page_url_kodi = get_url(action='videos', url=url_base_for_next_page, page=str(next_page), is_search='true')
+        else:
+            url_base_for_next_page = url.split('/page:')[0] if '/page:' in url else url
+            next_page_url_kodi = get_url(action='videos', url=url_base_for_next_page, page=str(next_page))
+        
+        xbmcplugin.addDirectoryItem(_handle, next_page_url_kodi, list_item, True)
 
     xbmcplugin.endOfDirectory(_handle)
 
@@ -275,6 +312,21 @@ def play_video(url):
         xbmcgui.Dialog().ok('Spryciarze.pl', 'Nie mozna znalezc filmu.')
         xbmcplugin.setResolvedUrl(_handle, False, listitem=xbmcgui.ListItem())
 
+def do_search():
+    keyboard = xbmc.Keyboard('', 'Wprowadz zapytanie wyszukiwania')
+    keyboard.doModal()
+    if keyboard.isConfirmed():
+        search_query = keyboard.getText()
+        if search_query:
+            xbmc.log(f"Wyszukiwanie: {search_query}", xbmc.LOGINFO)
+            search_url = f"{BASE_URL}/szukaj/{search_query.replace(' ', '+')}"
+            plugin_url = get_url(action='videos', url=search_url, is_search='true')
+            xbmc.executebuiltin(f'Container.Update({plugin_url})')
+        else:
+            xbmcgui.Dialog().notification('Spryciarze.pl', 'Zapytanie wyszukiwania nie moze byc puste.', xbmcgui.NOTIFICATION_ERROR)
+    else:
+        xbmcgui.Dialog().notification('Spryciarze.pl', 'Wyszukiwanie anulowane.', xbmcgui.NOTIFICATION_INFO)
+
 def router(paramstring):
     params = dict(parse_qsl(paramstring))
 
@@ -284,9 +336,12 @@ def router(paramstring):
         list_subcategories(params['url'])
     elif params['action'] == 'videos':
         page = int(params.get('page', 1))
-        list_videos(params['url'], page)
+        is_search = params.get('is_search', 'false').lower() == 'true'
+        list_videos(params['url'], page, is_search)
     elif params['action'] == 'play':
         play_video(params['url'])
+    elif params['action'] == 'search':
+        do_search()
     else:
         raise ValueError('Nieprawidlowy parametr: ' + paramstring)
 
